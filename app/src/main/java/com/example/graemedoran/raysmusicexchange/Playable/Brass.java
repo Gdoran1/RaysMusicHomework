2a9f6a70f6d36aabbb80ca22fd919249b0992076
graeme doran <graeme42@icloud.com>
2017-11-03 13:15:59 +0000

    start file structure

---
+++ app/src/main/java/com/example/graemedoran/raysmusicexchange/Playable/Brass.java
@@ -0,0 +1,9 @@
+package com.example.graemedoran.raysmusicexchange.Playable;
+
+/**
+ * Created by graemedoran on 03/11/2017.
+ */
+
+public class Brass {
+
+}
---
+++ app/src/main/java/com/example/graemedoran/raysmusicexchange/Playable/String.java
@@ -0,0 +1,8 @@
+package com.example.graemedoran.raysmusicexchange.Playable;
+
+/**
+ * Created by graemedoran on 03/11/2017.
+ */
+
+public class String {
+}
